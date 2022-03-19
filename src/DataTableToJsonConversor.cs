75aa6e8b007a17ae799fefff727f1687f8f27231
Jose Antonio Bautista Montejo <jbautistam@gmail.com>
2022-03-19 13:26:15 +0100

    Agregar archivos de proyecto.

---
+++ src/DataTableToJsonConversor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bau.Libraries.LibJsonConversor
+{
+	/// <summary>
+	///		Conversor de un <see cref="DataTable"/> a una cadena JSon
+	/// </summary>
+    public class DataTableToJsonConversor
+    {
+		/// <summary>
+		///		Convierte una tabla a JSon
+		/// </summary>
+		public string ConvertToJson(DataTable table, int startRow = 0, int rowsNumber = 0)
+		{
+			if (table.Rows.Count == 0)
+				return ConvertHeaders(table);
+			else
+				return ConvertDataTable(table, startRow, rowsNumber);
+		}
+
+		/// <summary>
+		///		Convierte un <see cref="DataTable"/> con datos a JSon
+		/// </summary>
+		private string ConvertDataTable(DataTable table, int startRow = 0, int rowsNumber = 0)
+		{
+			JsonSerializer js = JsonSerializer.CreateDefault();
+			JArray rows = JArray.FromObject(table, js);
+
+				// Devuelve los datos de la tabla
+				if (startRow > 0 && rowsNumber > 0)
+					return rows.Skip(startRow).Take(rowsNumber)?.ToString();
+				else
+					return rows.ToString();
+		}
+
+		/// <summary>
+		///		Convierte las cabeceras de la tabla a JSon (la tabla no tiene ningún dato, el conversor a JSon devuelve una cadena vacía)
+		/// </summary>
+		private string ConvertHeaders(DataTable table)
+		{
+			var sb = new System.Text.StringBuilder();
+
+				// Cabecera
+				sb.Append("[{");
+				// Convierte las cabeceras de la tabla
+				foreach (DataColumn column in table.Columns)
+				{
+					sb.Append($"'{column.ColumnName}': null");
+					if (table.Columns.IndexOf(column) < table.Columns.Count - 1)
+						sb.Append(",");
+				}
+				// Cierre
+				sb.Append("}]");
+				// Devuelve la cadena
+				return sb.ToString();
+		}
+    }
+}
---
+++ src/JsonToDataTableConversor.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bau.Libraries.LibJsonConversor
+{
+	/// <summary>
+	///		Conversor de datos en Json a un DataTable
+	/// </summary>
+    public class JsonToDataTableConversor
+    {
+		/// <summary>
+		///		Convierte una cadena json a una tabla de datos
+		/// </summary>
+		public DataTable ConvertToDataTable(string json)
+		{
+			try
+			{
+				return ConvertToDataTable(JsonConvert.DeserializeObject<List<JObject>>(json));
+			}
+			catch
+			{
+				return ConvertToDataTable(new List<JObject> { JsonConvert.DeserializeObject<JObject>(json) });
+			}
+		}
+
+		/// <summary>
+		///		Convierte los elementos de un json en un DataTable
+		/// </summary>
+		public DataTable ConvertToDataTable(IEnumerable<object> items)
+		{
+			DataTable table = new DataTable();
+
+				// Recorre las filas
+				if (items != null)
+					foreach (object item in items)
+						if (item is JObject jsonRecord && jsonRecord != null)
+						{
+							DataRow row = table.NewRow();
+
+								// Añade los datos de la fila
+								foreach (KeyValuePair<string, JToken> keyToken in jsonRecord)
+								{
+									// Crea la columna si no existía
+									if (table.Columns.IndexOf(keyToken.Key) < 0)
+									{
+										DataColumn column = table.Columns.Add(keyToken.Key, ConvertJsonType(keyToken.Value.Type));
+
+											// Asigna las propiedades adicionales de la columna
+											column.AllowDBNull = true;
+									}
+									// Le asigna el valor
+									row[keyToken.Key] = ConvertJsonObject(keyToken.Value.Type, keyToken.Value);
+								}
+								// Añade la fila
+								table.Rows.Add(row);
+						}
+				// Devuelve la tabla creada
+				return table;
+		}
+
+		/// <summary>
+		///		Convierte un tipo de Json a un tipo de .Net
+		/// </summary>
+		private Type ConvertJsonType(JTokenType type)
+		{
+			switch (type)
+			{ 
+				case JTokenType.Integer:
+					return typeof(int);
+				case JTokenType.Float:
+					return typeof(double);
+				case JTokenType.String:
+					return typeof(string);
+				case JTokenType.Boolean:
+					return typeof(bool);
+				case JTokenType.Date:
+					return typeof(DateTime);
+				case JTokenType.Bytes:
+					return typeof(byte[]);
+				case JTokenType.Guid:
+					return typeof(Guid);
+				case JTokenType.Uri:
+					return typeof(Uri);
+				case JTokenType.TimeSpan:
+					return typeof(TimeSpan);
+				default: // JTokenType: None, Object, Array, Constructor, Property, Comment, Null, Undefined, Raw:
+					return typeof(string);
+					// throw new ArgumentException("Type unknown");
+			}
+		}
+
+		/// <summary>
+		///		Convierte un valor de Json en un valor .Net
+		/// </summary>
+		private object ConvertJsonObject(JTokenType? type, JToken value)
+		{
+			switch (type)
+			{
+				case JTokenType.Integer:
+					return (int?) value;
+				case JTokenType.Float:
+					return (float?) value;
+				case JTokenType.String:
+					return (string) value;
+				case JTokenType.Boolean:
+					return (bool) value;
+				case JTokenType.Date:
+					return (DateTime?) value;
+				case JTokenType.Bytes:
+					return (byte[]) value;
+				case JTokenType.Guid:
+					return (Guid?) value;
+				case JTokenType.Uri:
+					return (Uri) value;
+				case JTokenType.TimeSpan:
+					return (TimeSpan?) value;
+				case JTokenType.Null:
+					return DBNull.Value;
+				default:
+					return null;
+			}
+		}
+    }
+}
+
